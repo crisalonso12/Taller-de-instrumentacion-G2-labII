8f17a77a85dbf3bfa26ecec4a477bf8b6e18001a
Diego Trejos <100897371+diegotm97@users.noreply.github.com>
2023-09-18 18:33:27 -0600

    Add files via upload

--- Sims/adc.ino
+++ Sims/adc.ino
@@ -1,26 +1,41 @@
 void setup() {
-  // Configura el pin A0 como entrada analógica
   pinMode(A0, INPUT);
-  // Inicializa la comunicación serial
+  pinMode(A1, INPUT);
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  // Tiempo entre muestras en microsegundos (200 microsegundos para 5000 muestras por segundo)
+  
+  // Tiempo entre muestras en microsegundos (200 microsegundos para 2500 muestras por segundo)
   const unsigned long tiempo_entre_muestras = 200;
 
-  // Realiza la conversión a voltaje a una tasa de 5000 muestras por segundo
+  //conversión a voltaje a una tasa de 2500 muestras por segundo para cada canal
   unsigned long tiempo_inicio = micros();
   while (micros() - tiempo_inicio < tiempo_entre_muestras) {
-    int valor = analogRead(A0);
-    // Convierte el valor leído a voltaje (en este caso, con referencia de 5V)
-    float voltaje = (valor / 1023.0) * 5.0;
-    // Imprime el voltaje en el monitor serial
-    Serial.print("Voltaje: ");
-    Serial.print(voltaje);
+    
+    // primer canal (A0)
+    int valor_canal_0 = analogRead(A0);
+    
+    float voltaje_canal_0 = (valor_canal_0 / 1023.0) * 5.0;
+    voltaje_canal_0 = (voltaje_canal_0 - 2.5) * 1.0;
+    
+    // segundo canal (A1)
+    int valor_canal_1 = analogRead(A1);
+    
+    float voltaje_canal_1 = (valor_canal_1 / 1023.0) * 5.0;
+    voltaje_canal_1 = (voltaje_canal_1 - 2.5) * 1.0;
+    
+  
+    Serial.print("Canal 1: ");
+    Serial.print(voltaje_canal_0);
+    Serial.println(" V");
+    
+    Serial.print("Canal 2: ");
+    Serial.print(voltaje_canal_1);
     Serial.println(" V");
   }
 
-  // Espera un breve período de tiempo para mantener la tasa de muestreo
+  
   delay(1);
 }
